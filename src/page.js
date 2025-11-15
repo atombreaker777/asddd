571bd81eda65932491a0c5a542dd3c06bad4ace3
atombreaker777 <opiumspoofer@gmail.com>
2025-11-15 14:11:48 +0100

    Delete src/page.js

--- src/page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Mosoly Dental API</h1>
-      <p>This application provides API routes for the Mosoly Dental digital receptionist.</p>
-    </div>
-  );
-}
